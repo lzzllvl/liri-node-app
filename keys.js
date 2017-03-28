d1b3c08b0217efd15c8ae12fc7ed57620eb80f14
Greg Lee <gml2136@gmail.com>
2017-03-27 20:05:01 -0700

    added the app tokens from twitter to keys.js

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('keys.js is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'tnbFtK2f3IVk9jKgq8QlVs9SL',
+  consumer_secret: 'GARn31ThED6BW1yC4Rj6gLUp28ObvzgBVywTmsViBFbMzvjM22',
+  access_token_key: '846538431672926208-wflUrR3ksKLy1WAfTXEPwnJSqZLCjgJ',
+  access_token_secret: '	HTEsPZRNFPTw1dmQi6oGD4BCaS0dVguBP7omWXlSGGUHK',
+}
--- liri.js
+++ liri.js
@@ -1,1 +1,5 @@
-"" 
+var twitter = require('twitter')
+var twitterKeys = requrie('twitterKeys');
+var fs = require('fs');
+var request = require('request');
+var spotify = require('spotify');
