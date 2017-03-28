00aadc9e191808a0fdd5a00e447d78ef6489fc3f
Greg Lee <gml2136@gmail.com>
2017-03-28 00:19:36 -0700

    changed the cascading if statements to a switch statement, added default case for invalid inputs

--- liri.js
+++ liri.js
@@ -1,3 +1,12 @@
+/**
+ * Node App For the interacting with twitter, spotify and omdb from the command
+ * line. Accepts Command line arguments in the format
+ * `node liri.js <function> "<arguments>"`
+ * Author - Gregory Lee for UCSD Coding bootcamp
+ */
+
+
+//require node modules
 var twitter = require('twitter')
 var twitterKeys = require('./keys.js');
 var fs = require('fs');
@@ -5,19 +14,12 @@ var request = require('request');
 var spotify = require('spotify');
 
 
-/**
- * Node App For the interacting with twitter, spotify and omdb from the command
- * line. Accepts Command line arguments in the format
- * `node liri.js <function> "<arguments>"`
- *
- */
 
 //TODO need to add resiliency to the program for the misuse cases.
 
 
 
-//functions
-
+//function declarations
 var getTweets = () => {
   console.log("running getTweets()");
 }
@@ -35,13 +37,28 @@ var doRandom = () => {
 }
 
 
+
+//script
+var usage = "Usage: node liri.js <process-name> 'process arguments'\n"
 var args = process.argv.slice(2);
 var func = args[0];
 
-if(func == "my-tweets") getTweets();
-if(func == "movie-this") getMovie(args[1]);
-if(func == "spotify-this-song") spotifyThis(args[1]);
-if(func == "do-what-it-says") doRandom();
-
+switch(func){
+  case "my-tweets":
+    getTweets();
+    break;
+  case "movie-this":
+    getMovie(args[1]);
+    break;
+  case "spotify-this-song":
+    spotifyThis(args[1]);
+    break;
+  case "do-what-it-says":
+    doRandom();
+    break;
+  default:
+    console.log("Invalid Input: '" + func + "' is not a valid process-name");
+    console.log(usage);
+}
 
 
