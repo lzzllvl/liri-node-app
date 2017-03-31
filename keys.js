ad8eed561d6b6b91e8eeb48b068937a82bc2ff19
Greg Lee <gml2136@gmail.com>
2017-03-30 18:52:20 -0700

    implemented a switcher function

--- keys.js
+++ keys.js
@@ -4,5 +4,5 @@ exports.twitterKeys = {
   consumer_key: 'tnbFtK2f3IVk9jKgq8QlVs9SL',
   consumer_secret: 'GARn31ThED6BW1yC4Rj6gLUp28ObvzgBVywTmsViBFbMzvjM22',
   access_token_key: '846538431672926208-wflUrR3ksKLy1WAfTXEPwnJSqZLCjgJ',
-  access_token_secret: '	HTEsPZRNFPTw1dmQi6oGD4BCaS0dVguBP7omWXlSGGUHK',
+  access_token_secret: 'HTEsPZRNFPTw1dmQi6oGD4BCaS0dVguBP7omWXlSGGUHK',
 }
--- liri.js
+++ liri.js
@@ -8,31 +8,45 @@
 
 
 //require node modules
-var twitter = require('twitter')
-var twitterKeys = require('./keys.js');
-var fs = require('fs');
-var request = require('request');
-var spotify = require('spotify');
-
-
+let twitter = require('twitter')
+let twitterKeys = require('./keys.js');
+let fs = require('fs');
+let request = require('request');
+let spotify = require('spotify');
 
 //TODO need to add resiliency to the program for the misuse cases.
 
-
-
 //function declarations
-var getTweets = () => {
+let getTweets = (number=20) => {
   console.log("running getTweets()");
+  var client = new twitter(twitterKeys.twitterKeys);
+  var params = {
+                  screen_name: 'Korn_E_Ninja',
+                  count: number
+               };
+  client.get('statuses/user_timeline', params, (error, tweets, response) => {
+    if(!error){
+      tweets.forEach(val => {
+        var date = val.created_at.split("+")[0].trim();
+        var logString = `By: ${val.user.name}\n`
+                      + `Text: ${val.text}\n`
+                      + `Created On: ${date}\n`
+        console.log(logString);
+      });
+    } else {
+      console.log(error)
+    }
+  })
 }
 
-var getMovie = (movieName="Mr. Nobody") => {
+let getMovie = (movieName="Mr. Nobody") => {
   console.log("running getMovie(" + movieName + ")");
   movieName  = movieName.replace(" ", "+");
-  var url = "http://www.omdbapi.com/?t=" + movieName;
+  let url = "http://www.omdbapi.com/?t=" + movieName;
   request(url, (err, res, body) => {
     if(!err && res.statusCode === 200){
-      var mov = JSON.parse(body);
-      var rottenTomatoes = mov.Ratings[1].Value;
+      let mov = JSON.parse(body);
+      let rottenTomatoes = mov.Ratings[1].Value;
       console.log(mov.Title, mov.Year);
       console.log(mov.imdbRating);
       console.log(mov.Country);
@@ -40,44 +54,63 @@ var getMovie = (movieName="Mr. Nobody") => {
       console.log(mov.Plot);
       console.log(mov.Actors);
       console.log("Rotten Tomatoes - " + rottenTomatoes);
-
     } else {
-      console.log(err);
+      console.log("Status Code: " + res.statusCode, err);
     }
   })
 }
 
-var spotifyThis = (songName="The Sign") => {
+let spotifyThis = (songName="The Sign") => {
   console.log("running spotifyThis(" + songName + ")");
+  var queryString = "https://api.spotify.com/v1/search?q="
+                      + songName.replace(" ", "+")
+                      + "&type=track";
+  spotify.get(queryString, (err, data) => {
+    if(!err) {
+      console.log(data.tracks.items[0].album.artists);
+      console.log(data.tracks.items[0]);
+      //console.log(JSON.stringify(data, null, 2));
+      //console.log(JSON.parse(data, null, 2));
+    } else {
+      //console.log(err);
+    }
+  });
 }
 
-var doRandom = () => {
+let doRandom = () => {
   console.log("running doRandom()");
+  fs.readFile('random.txt', 'utf8', (err, data) =>{
+    console.log(data);
+    var temp = data.split(", ");
+    var func = temp[0];
+    var arg = temp[1];
+    funcSwitcher(func, arg);
+  })
 }
 
-
+let funcSwitcher = (func, arg) => {
+  let usage = "Usage: node liri.js <process-name> 'process arguments'\n";
+  switch(func){
+    case "my-tweets":
+      getTweets(arg);
+      break;
+    case "movie-this":
+      getMovie(arg);
+      break;
+    case "spotify-this-song":
+      spotifyThis(arg);
+      break;
+    case "do-what-it-says":
+      doRandom();
+      break;
+    default:
+      console.log("Invalid Input: '" + func + "' is not a valid process-name");
+      console.log(usage);
+  }
+}
 
 //script
-var usage = "Usage: node liri.js <process-name> 'process arguments'\n"
-var args = process.argv.slice(2);
-var func = args[0];
-
-switch(func){
-  case "my-tweets":
-    getTweets();
-    break;
-  case "movie-this":
-    getMovie(args[1]);
-    break;
-  case "spotify-this-song":
-    spotifyThis(args[1]);
-    break;
-  case "do-what-it-says":
-    doRandom();
-    break;
-  default:
-    console.log("Invalid Input: '" + func + "' is not a valid process-name");
-    console.log(usage);
-}
-
+let args = process.argv.slice(2);
+let funcName = args[0];
+funcSwitcher(funcName, args[1]);
 
